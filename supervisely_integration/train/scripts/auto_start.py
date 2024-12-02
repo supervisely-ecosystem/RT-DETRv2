c210680240870c1727b9778c0e80f52ae6e2a217
cxnt <pavelbartsits@gmail.com>
2024-12-02 16:31:36 +0400

    update requirements and refactor auto_start script for RT-DETRv2 integration; add hyperparameters configuration

--- supervisely_integration/train/scripts/auto_start.py
+++ supervisely_integration/train/scripts/auto_start.py
@@ -8,35 +8,41 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 load_dotenv("local.env")
 
 api: sly.Api = sly.Api.from_env()
+
+task_id = 68841
+method = "train_from_api"
+
 hyperparameters_path = os.path.join(os.path.dirname(__file__), "hyperparameters.yaml")
 with open(hyperparameters_path, "r") as f:
     hyper_params = f.read()
     print(hyper_params)
 
-app_config = {
-    "input": {
-        "project_id": 42201,
-        "train_dataset_id": 99198,
-        "val_dataset_id": 99199,
-    },
-    "classes": ["cat", "dog"],
-    "model": {
-        # Pretrain
-        # "source": "Pretrained models",
-        # "model_name": "rtdetr_r50vd_coco_objects365",
-        # Custom
-        "source": "Custom models",
-        "task_id": "debug-session",
-        "checkpoint": "checkpoint0011.pth",
-    },
-    "hyperparameters": hyper_params,
-    "options": {
-        "model_benchmark": {
-            "enable": True,
-            "speed_test": True,
+data = {
+    "app_state": {
+        "input": {
+            "project_id": 43166,
+            "train_dataset_id": 101769,
+            "val_dataset_id": 101770,
         },
-        "cache_project": True,
-    },
+        "classes": ["cat", "dog"],
+        "model": {
+            # Pretrain
+            "source": "Pretrained models",
+            "model_name": "RT-DETRv2-S",
+            # Custom
+            # "source": "Custom models",
+            # "task_id": "debug-session",
+            # "checkpoint": "checkpoint0011.pth",
+        },
+        "hyperparameters": hyper_params,
+        "options": {
+            "model_benchmark": {
+                "enable": True,
+                "speed_test": True,
+            },
+            "cache_project": True,
+        },
+    }
 }
 
-api.app.send_request()
+api.app.send_request(task_id, method, data)
