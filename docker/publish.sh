e31473acf1ed0abcbd76b07f195cb5b8daa615b5
cxnt <pavelbartsits@gmail.com>
2024-12-10 13:26:06 +0400

    Update dependencies and Docker configurations for RT-DETRv2

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.0 . && \
-docker push supervisely/rt-detrv2:1.0.0
+docker build -t supervisely/rt-detrv2:1.0.2 . && \
+docker push supervisely/rt-detrv2:1.0.2
