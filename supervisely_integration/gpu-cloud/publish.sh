21acb6d278c5e62d8df0bbd594f0749be1edcdfe
cxnt <pavelbartsits@gmail.com>
2024-12-16 16:51:51 +0400

    Add GPU-Cloud Docker image build workflow and related files

---
+++ supervisely_integration/gpu-cloud/publish.sh
@@ -0,0 +1,2 @@
+docker build -t supervisely/rt-detrv2-gpu-cloud:1.0.3 . && \
+docker push supervisely/rt-detrv2-gpu-cloud:1.0.3
--- supervisely_integration/serve/main.py
+++ supervisely_integration/serve/main.py
@@ -1,8 +1,9 @@
 import os
+
 from dotenv import load_dotenv
+
 import supervisely as sly
 from supervisely_integration.serve.rtdetrv2 import RTDETRv2
-
 
 if sly.is_development():
     load_dotenv("local.env")
@@ -13,3 +14,15 @@ model = RTDETRv2(
     use_serving_gui_template=True,
 )
 model.serve()
+
+# docker run \
+#   --shm-size=1g \
+#   --runtime=nvidia \
+#   --cap-add NET_ADMIN \
+#   --env-file ~/supervisely.env \
+#   --env ENV=production \
+#   -v "./RT-DETRv2:/app" \
+#   -w /app \
+#   supervisely/rt-detrv2:1.0.3 \
+#   python3 -m uvicorn main:model.app --app-dir /app/RT-DETRv2/supervisely_integration/serve --host 0.0.0.0 --port 8000 --ws websockets -- \
+#   --model "RT-DETRv2-S" --predict "data/test" --output-dataset "Predictions/ds1" --workspace_id "349"
