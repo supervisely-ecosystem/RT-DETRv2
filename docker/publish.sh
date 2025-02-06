1d1b695d7b0fb27f17ba14f2576128d9e7467aa4
cxnt <pavelbartsits@gmail.com>
2025-02-06 19:42:31 +0400

    Update Dockerfile and configuration for RT-DETRv2 to version 1.0.8 and Supervisely to 6.73.299

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.7 . && \
-docker push supervisely/rt-detrv2:1.0.7
+docker build -t supervisely/rt-detrv2:1.0.8 . && \
+docker push supervisely/rt-detrv2:1.0.8
