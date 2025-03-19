62ac3b4305e9e6f7444325b22eb68fb11e9ec565
GoldenAnpu <seryios.sych@gmail.com>
2025-03-19 01:14:43 +0100

    Upgrade SDK to v6.73.334 and docker image to 1.0.14

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.13 . && \
-docker push supervisely/rt-detrv2:1.0.13
+docker build -t supervisely/rt-detrv2:1.0.14 . && \
+docker push supervisely/rt-detrv2:1.0.14
