4837999bc437e3fc3ad6a549a1346e510a1a419f
cxnt <pavelbartsits@gmail.com>
2025-07-08 15:35:55 +0400

    Refactor test_model.py to support multiple model configurations, update environment loading to use user-specific path, and modify prediction handling.

--- supervisely_integration/serve/scripts/test_model.py
+++ supervisely_integration/serve/scripts/test_model.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 import supervisely as sly
 
@@ -5,30 +6,32 @@ from supervisely_integration.serve.rtdetrv2 import RTDETRv2
 
 if sly.is_development():
     load_dotenv("local.env")
-    load_dotenv("supervisely.env")
+    # load_dotenv("supervisely.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+model_n = 2
 
 # 1. Pretrained model
-# model = RTDETRv2(
-#     model="RT-DETRv2-S",
-#     device="cuda",
-# )
+if model_n == 1:
+    model = RTDETRv2(
+        model="RT-DETRv2-S",
+        device="cuda",
+    )
 
 # 2. Local checkpoint
-# model_local = RTDETRv2(
-#     model="47705_RT-DETRv2/checkpoints/best.pth",
-#     device="cuda",
-# )
+elif model_n == 2:
+    model = RTDETRv2(
+        model="my_models/best.pth",
+        device="cuda",
+    )
 
 # 3. Remote Custom Checkpoint (Team Files)
-# model_remote = RTDETRv2(
-    # model="/experiments/9_Animals (Bitmap)/47705_RT-DETRv2/checkpoints/best.pth",
-    # device="cuda:0",
-# )
-
-model = RTDETRv2(
-    model="RT-DETRv2-S",
-    device="cuda",
-)
+elif model_n == 3:
+    model = RTDETRv2(
+        model="/experiments/9_Animals (Bitmap)/47705_RT-DETRv2/checkpoints/best.pth",
+        device="cuda:0",
+    )
 
 image_path = "supervisely_integration/demo/img/coco_sample.jpg"
-model(image_path)
+predictions = model(input=image_path)
+print(f"Predictions: {len(predictions)}")
