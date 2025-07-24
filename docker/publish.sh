51312c54fda911aee0946345dc0438dd7d97dffe
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-07-24 16:06:43 +0400

    Update Supervisely SDK version to v6.73.405 (#26)

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.26 . && \
-docker push supervisely/rt-detrv2:1.0.26
+docker build -t supervisely/rt-detrv2:1.0.28 . && \
+docker push supervisely/rt-detrv2:1.0.28
