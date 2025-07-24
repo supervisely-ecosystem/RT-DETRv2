ce33560a11aca4fea39e1f333b26e4f3be319fae
cxnt <pavelbartsits@gmail.com>
2025-07-24 12:01:52 +0000

    Update Supervisely SDK to v6.73.405 and Docker image version to v1.0.28 across configuration files

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.27 . && \
-docker push supervisely/rt-detrv2:1.0.27
+docker build -t supervisely/rt-detrv2:1.0.28 . && \
+docker push supervisely/rt-detrv2:1.0.28
