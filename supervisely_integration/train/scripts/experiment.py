761f3458b4c8603d126d9d9c9f14f0e1806892e0
cxnt <pavelbartsits@gmail.com>
2025-08-11 19:51:47 +0400

    Update experiment configuration and dependencies

--- supervisely_integration/train/scripts/experiment.py
+++ supervisely_integration/train/scripts/experiment.py
@@ -16,46 +16,48 @@ team_id = sly.env.team_id()
 workspace_id = sly.env.workspace_id()
 
 experiment_info = {
-	"experiment_name": "49321_Animals (Rectangle)_RT-DETRv2-M",
+	"experiment_name": "49487_Animals (Rectangle) (Old)_RT-DETRv2-S",
 	"framework_name": "RT-DETRv2",
-	"model_name": "RT-DETRv2-M",
-	"base_checkpoint": "rtdetrv2_r50vd_m_7x_coco_ema.pth",
-	"base_checkpoint_link": "https://github.com/lyuwenyu/storage/releases/download/v0.1/rtdetrv2_r50vd_m_7x_coco_ema.pth",
+	"model_name": "RT-DETRv2-S",
+	"base_checkpoint": "rtdetrv2_r18vd_120e_coco_rerun_48.1.pth",
+	"base_checkpoint_link": "https://github.com/lyuwenyu/storage/releases/download/v0.2/rtdetrv2_r18vd_120e_coco_rerun_48.1.pth",
 	"task_type": "object detection",
-	"project_id": 1322,
-	"task_id": 49321,
+	"project_id": 30,
+	"project_version": {
+		"id": 222,
+		"version": 4
+	},
+	"task_id": 49487,
 	"model_files": {
 		"config": "model_config.yml"
 	},
 	"checkpoints": [
 		"checkpoints/best.pth",
-		"checkpoints/checkpoint0040.pth",
-		"checkpoints/checkpoint0080.pth",
+		"checkpoints/checkpoint0005.pth",
 		"checkpoints/last.pth"
 	],
 	"best_checkpoint": "best.pth",
-	"export": {
-		"ONNXRuntime": "export/best.onnx",
-		"TensorRT": "export/best.engine"
-	},
+	"export": {},
 	"app_state": "app_state.json",
 	"model_meta": "model_meta.json",
 	"hyperparameters": "hyperparameters.yaml",
-	"artifacts_dir": "/experiments/1322_Animals (Rectangle)/49321_RT-DETRv2/",
-	"datetime": "2025-08-07 10:48:42",
+	"artifacts_dir": "/experiments/30_Animals (Rectangle) (Old)/49487_RT-DETRv2/",
+	"datetime": "2025-08-11 15:42:16",
 	"evaluation_report_id": None,
 	"evaluation_report_link": None,
 	"evaluation_metrics": {},
 	"primary_metric": None,
 	"logs": {
 		"type": "tensorboard",
-		"link": "/experiments/1322_Animals (Rectangle)/49321_RT-DETRv2/logs/"
+		"link": "/experiments/30_Animals (Rectangle) (Old)/49487_RT-DETRv2/logs/"
 	},
 	"device": "NVIDIA GeForce RTX 4090",
-	"training_duration": 251.07538713607937,
+	"training_duration": 9.387736642998789,
+	"train_collection_id": 530,
+	"val_collection_id": 531,
 	"train_val_split": "train_val_split.json",
-	"train_size": 27,
-	"val_size": 27
+	"train_size": 12,
+	"val_size": 12
 }
 
 model_meta = {
@@ -66,7 +68,7 @@ model_meta = {
 			"shape": "rectangle",
 			"color": "#A80B10",
 			"geometry_config": {},
-			"id": 32406,
+			"id": 78,
 			"hotkey": ""
 		},
 		{
@@ -75,7 +77,7 @@ model_meta = {
 			"shape": "rectangle",
 			"color": "#B8E986",
 			"geometry_config": {},
-			"id": 32407,
+			"id": 79,
 			"hotkey": ""
 		},
 		{
@@ -84,7 +86,7 @@ model_meta = {
 			"shape": "rectangle",
 			"color": "#9F21DE",
 			"geometry_config": {},
-			"id": 32408,
+			"id": 80,
 			"hotkey": ""
 		},
 		{
@@ -93,7 +95,7 @@ model_meta = {
 			"shape": "rectangle",
 			"color": "#1EA49B",
 			"geometry_config": {},
-			"id": 32409,
+			"id": 81,
 			"hotkey": ""
 		},
 		{
@@ -102,7 +104,7 @@ model_meta = {
 			"shape": "rectangle",
 			"color": "#F8E71C",
 			"geometry_config": {},
-			"id": 32410,
+			"id": 82,
 			"hotkey": ""
 		}
 	],
@@ -116,7 +118,7 @@ model_meta = {
 				"adult",
 				"senior"
 			],
-			"id": 4781,
+			"id": 62,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -126,7 +128,7 @@ model_meta = {
 			"name": "animal age group_1",
 			"value_type": "any_string",
 			"color": "#8A0F59",
-			"id": 4782,
+			"id": 63,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -136,7 +138,7 @@ model_meta = {
 			"name": "animal count",
 			"value_type": "any_number",
 			"color": "#E3BE1C",
-			"id": 4783,
+			"id": 64,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -146,7 +148,7 @@ model_meta = {
 			"name": "cat",
 			"value_type": "none",
 			"color": "#A80B10",
-			"id": 4784,
+			"id": 65,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -156,7 +158,7 @@ model_meta = {
 			"name": "dog",
 			"value_type": "none",
 			"color": "#B8E986",
-			"id": 4785,
+			"id": 66,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -166,7 +168,7 @@ model_meta = {
 			"name": "horse",
 			"value_type": "none",
 			"color": "#9F21DE",
-			"id": 4786,
+			"id": 67,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -176,7 +178,7 @@ model_meta = {
 			"name": "imgtag",
 			"value_type": "none",
 			"color": "#FF03D6",
-			"id": 4787,
+			"id": 68,
 			"hotkey": "",
 			"applicable_type": "imagesOnly",
 			"classes": [],
@@ -186,7 +188,7 @@ model_meta = {
 			"name": "sheep",
 			"value_type": "none",
 			"color": "#1EA49B",
-			"id": 4788,
+			"id": 69,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
@@ -196,7 +198,7 @@ model_meta = {
 			"name": "squirrel",
 			"value_type": "none",
 			"color": "#F8E71C",
-			"id": 4789,
+			"id": 70,
 			"hotkey": "",
 			"applicable_type": "all",
 			"classes": [],
