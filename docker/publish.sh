f26f2414b22fca3ee67e2bd3347b552a1044a407
cxnt <pavelbartsits@gmail.com>
2025-08-27 14:31:49 +0400

    Update Dockerfile and configuration files to use the latest development image tag for RT-DETRv2

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.32 . && \
-docker push supervisely/rt-detrv2:1.0.32
+docker build -t supervisely/rt-detrv2:dev . && \
+docker push supervisely/rt-detrv2:dev
