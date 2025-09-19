b89c364c885c1e3860f0f3107865b4a88878123c
cxnt <pavelbartsits@gmail.com>
2025-09-19 11:59:07 +0000

    Update Dockerfile to support CUDA 12.8

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:dev . && \
-docker push supervisely/rt-detrv2:dev
+docker build -t supervisely/rt-detrv2:1.0.35 . && \
+docker push supervisely/rt-detrv2:1.0.35
