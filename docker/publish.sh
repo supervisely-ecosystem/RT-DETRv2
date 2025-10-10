3ea1f890fb39c1ea94d3f6f0cbf1700fd70f714d
cxnt <pavelbartsits@gmail.com>
2025-10-10 15:13:15 +0400

    Update Docker image and Supervisely SDK version to 6.73.452; add new entries to .gitignore

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.37 . && \
-docker push supervisely/rt-detrv2:1.0.37
+docker build -t supervisely/rt-detrv2:1.0.38 . && \
+docker push supervisely/rt-detrv2:1.0.38
