d14e64d4260e8611c732ce7be06439fd37eadd29
vorozhkog <vorozhkog@icloud.com>
2026-01-16 05:39:43 +0000

    init

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.41 . && \
-docker push supervisely/rt-detrv2:1.0.41
+docker build -t supervisely/rt-detrv2:1.0.42 . && \
+docker push supervisely/rt-detrv2:1.0.42
