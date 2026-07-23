f4fac03cffc25e38dced4977278689c863f8dca9
Almaz <79905215+almazgimaev@users.noreply.github.com>
2026-07-23 22:35:30 +0200

    Update Docker image tag to 1.0.47

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.46 . && \
-docker push supervisely/rt-detrv2:1.0.46
+docker build -t supervisely/rt-detrv2:1.0.47 . && \
+docker push supervisely/rt-detrv2:1.0.47
