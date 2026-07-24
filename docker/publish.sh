90539dd2ad47fefd2d52ac60a1ee15bf4bcc04f3
almaz <almazgimaev@gmail.com>
2026-07-24 13:35:06 +0200

    docker: set RELEASE_VERSION for SDK git-branch build; label 6.74.16; publish :dev

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/rt-detrv2:1.0.48 . && \
-docker push supervisely/rt-detrv2:1.0.48
+docker build -t supervisely/rt-detrv2:dev . && \
+docker push supervisely/rt-detrv2:dev
